9066ee205f7c3e8f67a91d138ffb381267bda5ee
ricardus1999 <104752701+ricardus1999@users.noreply.github.com>
2022-05-25 22:40:42 +0900

    Create player.c

---
+++ player.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include "linkedlist.h"
+#include "textfilewriter.h"
+
+void read_command();
+
+int main() {
+    int number_of_song = 0;
+    int number_of_command = 0;
+
+    char* list[100] = {};
+
+    scanf("%d", number_of_song);
+    for(int i=0; i<number_of_song; i++) {
+	char music_name[100];
+	scanf("%s", music_name);
+	list[i] = music_name;
+    }
+
+    scanf("%d", number_of_command);
+    for(int i=0; i<number_of_command; i++) {
+        read_command();
+    }
+    return 0;
+}
+
+void read_command() {
+    char command[100];
+    scanf("%s", command);
+    if(!strcmp(command, "add"))        { add(); } 
+    else if(!strcmp(command, "del"))   { del(); } 
+    else if(!strcmp(command, "list"))  { list(); }
+    else if(!strcmp(command, "next"))  { next(); }
+    else if(!strcmp(command, "prev"))  { prev(); }
+    else if(!strcmp(command, "move"))  { move(); } 
+    else if(!strcmp(command, "play"))  { play(); }
+    else if(!strcmp(command, "clear")) { clear(); }
+    else if(!strcmp(command, "quit"))  { quit(); }
+    else if(!strcmp(command, "load"))  { load(); } 
+    else if(!strcmp(command, "save"))  { save(); } 
+    else { ; }
+}
